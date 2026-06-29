b3e6812a48c1b0c172af2fcbd69af1bd917bcbf4
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-06-29 12:00:56 +0545

    Add CORS configuration for URL shortener

---
+++ src/main/java/com/eikeyal/urlshortener/config/CrosConfig.java
@@ -0,0 +1,36 @@
+package com.eikeyal.urlshortener.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfig {
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "https://url-shortener-mu-inky.vercel.app"
+                        )
+                        .allowedMethods(
+                            "GET",
+                            "POST",
+                            "PUT",
+                            "DELETE",
+                            "OPTIONS"
+                        )
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
